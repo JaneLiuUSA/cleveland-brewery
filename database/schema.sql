2d99828d5df89b78a39ca7e616934bfe71b50845
Josh Wygle <jwygle@gmail.com>
2017-11-16 15:14:00 -0500

    commit for updated sql schema and test data

--- database/schema.sql
+++ database/schema.sql
@@ -4,23 +4,23 @@
 
 BEGIN;
 
-CREATE TABLE role {
+CREATE TABLE roles (
     role_id serial,
     role_name varchar(30),
     CONSTRAINT pk_role_role_id PRIMARY KEY (role_id)
-};
+);
 
-CREATE TABLE user{
+CREATE TABLE users (
     user_id serial,
     username varchar(30) NOT NULL,
     password varchar(100) NOT NULL,
     salt varchar(255),
-    role_id int,
+    role_id integer,
     CONSTRAINT pk_user_user_id PRIMARY KEY (user_id),
-    CONSTRAINT fk_role_role_id FOREIGN KEY (role_id)
-};
+    CONSTRAINT fk_role_role_id FOREIGN KEY (role_id) REFERENCES roles(role_id)
+);
 
-CREATE TABLE breweries {
+CREATE TABLE breweries (
     brewery_id serial,
     name varchar(255) NOT NULL,
     location varchar(255) NOT NULL,
@@ -28,35 +28,35 @@ CREATE TABLE breweries {
     img_url varchar(255),
     website_url varchar(255),
     business_hours varchar(255),
-    user_id NOT NULL
+    user_id bigint,
     CONSTRAINT pk_brewery_brewery_id PRIMARY KEY (brewery_id),
-    CONSTRAINT fk_user_user_id FOREIGN KEY (user_id)
-};
+    CONSTRAINT fk_user_user_id FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
 
-CREATE TABLE beer {
+CREATE TABLE beers (
     beer_id serial,
     name varchar(255) NOT NULL,
     abv decimal,
-    ibu int,
+    ibu integer,
     type varchar(255) NOT NULL,
     info varchar(255) NOT NULL,
     img_url varchar(255),
-    brewery_id int,
+    brewery_id integer,
     CONSTRAINT pk_beer_beer_id PRIMARY KEY (beer_id),
-    CONSTRAINT fk_brewery_brewery_id FOREIGN KEY (brewery_id)
-};
+    CONSTRAINT fk_brewery_brewery_id FOREIGN KEY (brewery_id) REFERENCES breweries(brewery_id)
+);
 
-CREATE TABLE reviews {
+CREATE TABLE reviews (
     review_id serial,
     subject varchar(100) NOT NULL,
     description text NOT NULL,
     rating int NOT NULL,
     create_date timestamp NOT NULL DEFAULT NOW(),
-    beer_id int,
-    user_id int,
+    beer_id integer,
+    user_id integer,
     CONSTRAINT pk_review_review_id PRIMARY KEY (review_id),
-    CONSTRAINT fk_beer_beer_id FOREIGN KEY (beer_id),
-    CONSTRAINT fk_user_user_id FOREIGN KEY (user_id)
-};
+    CONSTRAINT fk_beer_beer_id FOREIGN KEY (beer_id) REFERENCES beers(beer_id),
+    CONSTRAINT fk_user_user_id FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
 
 COMMIT;
