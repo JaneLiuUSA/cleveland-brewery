3800a617057844a3b42d51a6a073f6822da6c024
Josh Wygle <jwygle@gmail.com>
2017-11-17 10:19:45 -0500

    Commit for working sign up

--- src/main/java/com/techelevator/clebrews/controller/UserController.java
+++ src/main/java/com/techelevator/clebrews/controller/UserController.java
@@ -2,14 +2,21 @@ package com.techelevator.clebrews.controller;
 
 import java.util.Map;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.techelevator.clebrews.model.User;
 import com.techelevator.clebrews.model.UserDAO;
 
 
@@ -24,18 +31,28 @@ public class UserController {
 	}
 
 	@RequestMapping(path="/users/new", method=RequestMethod.GET)
-	public String displayNewUserForm() {
+	public String displayNewUserForm(ModelMap modelHolder) {
+		if(! modelHolder.containsAttribute("newUser")) {
+			modelHolder.addAttribute("newUser", new User());
+		}
 		return "newUser";
 	}
 	
 	@RequestMapping(path="/users", method=RequestMethod.POST)
-	public String createUser(@RequestParam String userName, @RequestParam String password, ModelMap model) {
-		if(userDAO.searchForUsernameAndPassword(userName, password)) {
-			userDAO.saveUser(userName, password);
-			return "redirect:/index";
-		} else {
-			model.put("errorMessage", "This username alreadys exists");
+	public String createUser(@Valid @ModelAttribute("newUser") User newUser, BindingResult result, RedirectAttributes flash) {
+		
+		flash.addFlashAttribute("newUser", newUser);
+		
+		if(result.hasErrors()) {
+			flash.addFlashAttribute(BindingResult.MODEL_KEY_PREFIX + "newUser", result);
 			return "redirect:/users/new";
+		}
+		if(!userDAO.searchForUsernameAndPassword(newUser.getUserName(), newUser.getPassword())) { //TODO dao function search by username only
+			userDAO.saveUser(newUser.getUserName(), newUser.getPassword());
+			return "redirect:/index"; 
+		} else {
+				flash.addFlashAttribute("message", "This username alreadys exists");
+				return "redirect:/users/new";
 		}
 
 	}
--- src/main/java/com/techelevator/clebrews/model/User.java
+++ src/main/java/com/techelevator/clebrews/model/User.java
@@ -1,20 +1,23 @@
 package com.techelevator.clebrews.model;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotBlank;
 
 public class User {
+	
 	private String userName;
-	@Min(value=8, message="Password too short, must be at least 8 characters")
+	@Size(min=8, message="Password too short, must be at least 8 characters")
+	@NotBlank(message="You must have a password")
 	@Pattern.List({
-	@Pattern(regexp="[A-Z]", message="Must have a capital"),
-	@Pattern(regexp="[a-z]", message="Must have a lowercase"),
-	@Pattern(regexp="[0-9]", message="Must have a number")
+	@Pattern(regexp="^.*[A-Z].*$", message="Must have a capital"),
+	@Pattern(regexp="^.*[a-z].*$", message="Must have a lowercase"),
+	@Pattern(regexp="^.*[0-9].*$", message="Must have a number")
 	})
-	@NotNull(message="You must have a password")
+	
 	private String password;
-	private String confirmPassword;
+
 	private int roleId;
 	
 	public String getUserName() {
@@ -29,12 +32,7 @@ public class User {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	public String getConfirmPassword() {
-		return confirmPassword;
-	}
-	public void setConfirmPassword(String confirmPassword) {
-		this.confirmPassword = confirmPassword;
-	}
+
 	public int getRoleId() {
 		return roleId;
 	}
