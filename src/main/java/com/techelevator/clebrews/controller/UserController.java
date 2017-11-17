4fe241235f6ab67175d4e89ca0123f845fb882ca
Ken Peterson <kenpeterson3@gmail.com>
2017-11-17 09:14:08 -0500

    moring merge day 2

--- src/main/java/com/techelevator/clebrews/controller/UserController.java
+++ src/main/java/com/techelevator/clebrews/controller/UserController.java
@@ -4,11 +4,12 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.techelevator.clebrews.model.UserDAO;
 
@@ -29,12 +30,12 @@ public class UserController {
 	}
 	
 	@RequestMapping(path="/users", method=RequestMethod.POST)
-	public String createUser(@RequestParam String userName, @RequestParam String password, ModelMap model) {
+	public String createUser(@RequestParam String userName, @RequestParam String password, BindingResult result, RedirectAttributes flash) {
 		if(userDAO.searchForUsernameAndPassword(userName, password)) {
 			userDAO.saveUser(userName, password);
 			return "redirect:/index";
 		} else {
-			model.put("errorMessage", "This username alreadys exists");
+			flash.addFlashAttribute(BindingResult.MODEL_KEY_PREFIX + "errorMessage", "This username alreadys exists");
 			return "redirect:/users/new";
 		}
 
